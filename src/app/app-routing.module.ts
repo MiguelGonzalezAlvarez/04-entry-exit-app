1085f4f39ec66bb7406443be9d5486f60511e537
MiguelMuros <miggonzalv@gmail.com>
2023-05-08 23:04:21 +0200

    feat: configure basic routing for pages

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+
+import { LoginComponent } from "./auth/login/login.component";
+import { RegisterComponent } from "./auth/register/register.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+
+const routes = [
+    { path: '', component: DashboardComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: '**', redirectTo: '' }
+]
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+    declarations: [],
+    providers: [],
+})
+export class AppRoutingModule { }
