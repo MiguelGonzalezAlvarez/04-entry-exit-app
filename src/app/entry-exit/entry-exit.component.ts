b739e71a13fbd8f3a87ce20d959cfa8d09de8abe
MiguelMuros <miggonzalv@gmail.com>
2023-05-08 08:59:04 +0200

    feat: create first components

---
+++ src/app/entry-exit/entry-exit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entry-exit',
+  templateUrl: './entry-exit.component.html',
+  styleUrls: ['./entry-exit.component.css']
+})
+export class EntryExitComponent {
+
+}
