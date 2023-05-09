b56b0bde65db59ae45b438a3c85c7d38ae53a8b7
MiguelMuros <miggonzalv@gmail.com>
2023-05-09 12:55:16 +0200

    feat: add register form logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -13,6 +14,7 @@ import { DetailComponent } from './entry-exit/detail/detail.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
@@ -29,7 +31,8 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  registerForm!: FormGroup;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      user: new FormControl('user', [Validators.required]),
+      email: new FormControl('email', [Validators.required, Validators.email]),
+      password: new FormControl('password', Validators.required),
+    });
+  }
+
+  createAccount() {
+    console.log(this.registerForm.value);
+  }
 
 }
