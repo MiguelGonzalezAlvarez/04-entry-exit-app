3dc2279fea2f23a1a190b8cb8b7cd5ece990ca23
MiguelMuros <miggonzalv@gmail.com>
2023-08-09 10:20:28 +0200

    feat: loading items from firebase docs

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.reducer';
+import { Subscription, filter } from 'rxjs';
+import { EntryExitService } from '../services/entry-exit.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit, OnDestroy {
+  userSubscription: Subscription = new Subscription();
+
+  constructor(private reduxStore: Store<AppState>, private entryExitService: EntryExitService) { }
+
+  ngOnInit(): void {
+    this.userSubscription = this.reduxStore
+      .select('authInfo')
+      .pipe(filter(authInfo => authInfo?.user != null))
+      .subscribe(({ user }) => {
+        console.log(user);
+        this.entryExitService.initEntryExitListener(user!.uid);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+  }
 
 }
--- src/app/services/entry-exit.service.ts
+++ src/app/services/entry-exit.service.ts
@@ -3,13 +3,20 @@ import { Firestore, doc, setDoc } from '@angular/fire/firestore';
 
 import { EntryExit } from '../models/entry-exit.model';
 import { AuthService } from './auth.service';
+import { onSnapshot } from 'firebase/firestore';
 
 @Injectable({
     providedIn: 'root'
 })
 export class EntryExitService {
 
-    constructor(private fireStore: Firestore, private authService:AuthService) {}
+    constructor(private fireStore: Firestore, private authService: AuthService) { }
+
+    initEntryExitListener(uid: string): void {
+        onSnapshot(doc(this.fireStore, `${uid}/entry-exit`), (doc) => {
+            console.log(doc.data());
+        });
+    }
 
     createEntryExit(entryExit: EntryExit): Promise<void> {
         const userId = this.authService.getCurrentUserId();
